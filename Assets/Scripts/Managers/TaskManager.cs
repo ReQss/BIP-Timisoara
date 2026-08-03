5c2339b398d64ead33d728fa10b48391d129c30d
ReQss <46198811+ReQss@users.noreply.github.com>
2026-08-03 10:41:54 +0300

    taskmanmager

--- Assets/Scripts/Managers/TaskManager.cs
+++ Assets/Scripts/Managers/TaskManager.cs
@@ -1,17 +1,83 @@
-using UnityEngine;  
+using UnityEngine; 
+using System.Collections;
+using System.Collections.Generic;
+ 
+public enum TaskType
+{
+    Coffe,
+    Food,
+    CleaningToilet,
+    TakingPiss,
+}
+[System.Serializable]
 public class TaskItem
 {
     public string taskName;
     public string taskDescription;
-    
+    public TaskType taskType;
+}
+[System.Serializable]
+public class Job
+{
+    public int waitingTime;
+    public TaskItem assignedTask;
+    public int jobId;
+    public Job(int waitingTime, TaskItem assignedTask, int jobId)
+    {
+        this.waitingTime = waitingTime;
+        this.assignedTask = assignedTask;
+        this.jobId = jobId;
+    }
 }
 
 public class TaskManager : MonoBehaviour
 {
+    [SerializeField]
+    public List<TaskItem> taskList = new List<TaskItem>();
+    public List<Job> currentJobs = new List<Job>();
+    public Job lastJob;
+    
+    public int maxJobs = 5;
+    public float timeBetweenJobs = 25f;
+    public float timeLeftForJob = 0f;
+    int jobIdCounter = 0;
+    public Job GetRandomJob()
+    {
+        //waiting time random from 30 to 60s
+        int randomWaitingTime = Random.Range(30, 61);
+        TaskItem randomTask = GetRandomTask();
+        Job newJob = new Job(randomWaitingTime, randomTask, jobIdCounter++);
+        return newJob;
+    }
+    public TaskItem GetRandomTask()
+    {
+        int randomIndex = Random.Range(0, taskList.Count);
+        return taskList[randomIndex];
+    }
+    public IEnumerator StartRandomTasks()
+    {
+        while (true)
+        {
+            Job newJob = GetRandomJob();
+            if(currentJobs.Count < maxJobs)
+            {
+                currentJobs.Add(newJob);
+                lastJob = newJob;
+            }
+            else
+            {
+                //wait until a customer is removed from the list
+                yield return new WaitUntil(() => currentJobs.Count < maxJobs);
+
+            }
+            yield return new WaitForSeconds(timeBetweenJobs);
+
+        }
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(StartRandomTasks());
     }
 
     // Update is called once per frame
--- Assets/Scripts/UI/UIHandler.cs
+++ Assets/Scripts/UI/UIHandler.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 
+[System.Serializable]
+public class JobItemInList
+{
+    public Job job;
+    public TextMeshProUGUI jobDescription;
+    public TextMeshProUGUI timeLeft;
+}
 public class UIHandler : MonoBehaviour
 {
     public GameObject pauseMenu;
+    //5 elements for jobs
+    public List<JobItemInList> jobItemsInList = new List<JobItemInList>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     // Update is called once per frame
@@ -12,6 +25,39 @@ public class UIHandler : MonoBehaviour
         {
             pauseMenu.SetActive(!pauseMenu.activeSelf);
         }
+    }
+    public JobItemInList GetFirstFreeJobItemInList()
+    {
+        return jobItemsInList.Find(job => job.job == null);
+    }
+    public void AddJobTextToList(Job job)
+    {
+        JobItemInList newJobItem = GetFirstFreeJobItemInList();
+        newJobItem.job = job;
+        newJobItem.jobDescription.text = job.assignedTask.taskDescription;
+        newJobItem.timeLeft.text = job.waitingTime.ToString();
+    }
+    
+       
+    
+    public void RemoveJobTextFromList(int jobId)
+    {
+        JobItemInList jobToRemove = jobItemsInList.Find(job => job.job != null && job.job.jobId == jobId);
+        if (jobToRemove != null)
+        {
+            jobToRemove.job = null;
+            jobToRemove.jobDescription.text = "";
+            jobToRemove.timeLeft.text = "";
+        }
+        //reorganize the list and push items to the top of the list
+        //sort by job id
+        jobItemsInList.Sort((a, b) => 
+        {
+            if (a.job == null && b.job == null) return 0;
+            if (a.job == null) return 1;
+            if (b.job == null) return -1;
+            return a.job.jobId.CompareTo(b.job.jobId);
+        });
     }
     public void LoadScene(string sceneName)
     {
